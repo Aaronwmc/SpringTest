17fd0e2fdab20713866a8e77e1a88972581a423c
Aaronwmc <18998474944@163.com>
2015-11-07 13:03:27 +0800

    update

---
+++ src/com/atguigu/spring/spel/Main.java
@@ -0,0 +1,21 @@
+package com.atguigu.spring.spel;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ApplicationContext context =  new ClassPathXmlApplicationContext("com/atguigu/spring/spel/beans-spel.xml");
+		Address address = (Address) context.getBean("address");
+		System.out.println(address);
+		
+		Car car = (Car) context.getBean("car");
+		System.out.println(car);
+		
+		Person person = (Person) context.getBean("person");
+		System.out.println(person);
+	}
+
+}
